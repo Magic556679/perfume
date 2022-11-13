2ff829222377af62d83fe025682a809ad6f2e70e
李晏瑋 <magic556679@gmail.com>
2022-11-13 17:23:26 +0800

    refactor 移除註解

--- src/main.js
+++ src/main.js
@@ -4,17 +4,14 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
-// aos
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-// VeeValidate
 import {
   Form, Field, ErrorMessage, defineRule, configure,
 } from 'vee-validate';
 import AllRules from '@vee-validate/rules';
 import { localize, setLocale } from '@vee-validate/i18n';
 import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
-// sweetalert
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import App from './App.vue';
